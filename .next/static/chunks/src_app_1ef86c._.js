b4a74f859eed4dbac4ca8a7d20bcd707b375af42
Lakshmanun <Lakshminarayanans.yadav@gmail.com>
2025-02-04 16:05:29 +0530

    home changes

--- .next/static/chunks/src_app_1ef86c._.js
+++ .next/static/chunks/src_app_1ef86c._.js
@@ -344,7 +344,17 @@ function Header1({ variant }) {
                                     className: "cs_main_header_left",
                                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
                                         className: "cs_site_branding",
-                                        href: "/"
+                                        href: "/",
+                                        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
+                                            src: "/assets/images/logo/logo.svg",
+                                            alt: "img",
+                                            width: 177,
+                                            height: 54
+                                        }, void 0, false, {
+                                            fileName: "[project]/src/app/Components/Header/Header1.jsx",
+                                            lineNumber: 46,
+                                            columnNumber: 17
+                                        }, this)
                                     }, void 0, false, {
                                         fileName: "[project]/src/app/Components/Header/Header1.jsx",
                                         lineNumber: 45,
