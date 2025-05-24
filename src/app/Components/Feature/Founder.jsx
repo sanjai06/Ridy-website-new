9ec0280eadf0c2fdc099168ee6318cd51c3d3039
Lakshmanun <Lakshminarayanans.yadav@gmail.com>
2025-05-24 10:53:15 +0530

    images

--- src/app/Components/Feature/Founder.jsx
+++ src/app/Components/Feature/Founder.jsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-// import data from '../../Data/feature3.json';
 import SectionTitle from '../Common/SectionTitle';
 import cofounder from '../../../../public/assets/images/Founder/Founders__md.jpg'
 import founder from '../../../../public/assets/images/Founder/cofounder__ceo.jpg'
@@ -16,22 +15,6 @@ const Feature6 = () => {
                     ></SectionTitle>
                 </div>
                 <div className="row  cardmargintop ">
-
-                    {/* {data.map((item, index) => (
-                        <div key={index} className="col-xl-6 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".3s">
-                            <div className='founder'>
-                                <div className="icon">
-                                    <Image src={item.img} alt="img" width={200} height={200} />
-                                </div>
-                                <div className="Founder_content">
-                                    <h3>{item.desc}</h3>
-                                    <h1>{item.title}</h1>
-                                  
-                                </div>
-                            </div>
-                        </div>
-                    ))} */}
-
                     <div className="col-xl-6 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".3s">
                         <div className='founder'>
                             <div className="icon">
@@ -50,10 +33,7 @@ const Feature6 = () => {
                                 <Image src={cofounder} alt="img" width={200} height={200} />
                             </div>
                             <div className="Founder_content">
-                                <h3>
-                                    Co-Founder & CEO
-
-                                </h3>
+                                <h3>Co-Founder & CEO</h3>
                                 <h1>Nazneen A</h1>
 
                             </div>
